da9a143c9a15d57b951d840a4306cc102f4b7a83
vinicius1diniz <vinicius.diniz@germinare.org.br>
2023-09-27 15:38:26 -0300

    subindo api

---
+++ src/test/java/com/example/apipratica2709/Apipratica2709ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apipratica2709;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apipratica2709ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
